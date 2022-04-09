3594b811bf22922ade4835a24f9c8eaf73c4f5b7
imycod <317009160@qq.com>
2022-04-09 13:26:17 +0800

    扁平化函数,将函数功能单一原则

--- index.js
+++ index.js
@@ -6,31 +6,40 @@ const fs = require('fs');
 // console.log('file written!');
 
 // Non-blocking , asynchronous way 1.
-fs.readFile('./txt/start.txt', 'utf8', (err, data1) => { // nodejs 先读文件 然后立即执行 2.的代码, 当文件彻底读完后才执行 callback函数
+fs.readFile('./txt/start.txt', 'utf8', (err, data) => start(err, data));
+
+let fileNameList = [];
+
+function start(err, data1) {
   if (err) {
     console.log(err);
   } else {
-    const fileNameList = data1.split(',');
+    fileNameList = data1.split(',');
     console.log("fileNameList", fileNameList);
     fileNameList.forEach(fileName => {
-      fs.readFile(`./txt/${fileName.trim()}.txt`, 'utf8', (err, data2) => {
-        if (err) {
-          console.log(err);
-        } else {
-          fs.readFile(`./txt/${data2.trim()}.txt`, 'utf8', (err, data3) => {
-            if (err) return;
-            fs.writeFile('./txt/final.txt', `${data1}\n${data2}\n${data3}`, (err) => {
-              if (err) {
-                console.log('writeFile', err);
-              }
-              console.log('your file has been written');
-            });
-            console.log("data3", data3);
-          });
-        }
-      });
+      fs.readFile(`./txt/${fileName.trim()}.txt`, 'utf8', (err, data2) => read2(err, data2));
     });
   }
-});
-// 2.
-console.log("will read file!");
+}
+
+let readData2 = "";
+
+function read2(err, data2) {
+  if (err) {
+    console.log(err);
+  } else {
+    readData2 = data2;
+    fs.readFile(`./txt/${data2.trim()}.txt`, 'utf8', (err, data3) => read3(err, data3));
+  }
+}
+
+function read3(err, data3) {
+  if (err) return;
+  fs.writeFile('./txt/final.txt', `${fileNameList}\n${readData2}\n${data3}`, (err) => {
+    if (err) {
+      console.log('writeFile', err);
+    }
+    console.log('your file has been written');
+  });
+  console.log("data3", data3);
+}
