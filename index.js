0496824e1c3a311a783ee0c2bb672989139ae4fc
imycod <317009160@qq.com>
2022-04-09 13:03:57 +0800

    非阻塞代码编写方式

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
-const  readStr = fs.readFileSync('./.gitignore', 'utf8');
+// const  readStr = fs.readFileSync('./.gitignore', 'utf8');
+// console.log(readStr);
+// const outputStr=`this is what we know about the avocado: \n${readStr} \nCreated by: ${process.env.USER} on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', outputStr);
+// console.log('file written!');
 
-const outputStr=`this is what we know about the avocado: \n${readStr} \nCreated by: ${process.env.USER} on ${Date.now()}`;
-fs.writeFileSync('./output.txt', outputStr);
-console.log('file written!');
-
+// Non-blocking , asynchronous way 1.
+fs.readFile('./txt/start.txt', 'utf8', (err, data) => { // nodejs 先读文件 然后立即执行 2.的代码, 当文件彻底读完后才执行 callback函数
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+// 2.
+console.log("will read file!");
