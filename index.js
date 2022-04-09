af4bea5208ab6e377ecf63184822e1ee59afc562
imycod <317009160@qq.com>
2022-04-09 13:13:50 +0800

    多层嵌套读取文件

--- index.js
+++ index.js
@@ -7,11 +7,24 @@ const fs = require('fs');
 
 // Non-blocking , asynchronous way 1.
 fs.readFile('./txt/start.txt', 'utf8', (err, data) => { // nodejs 先读文件 然后立即执行 2.的代码, 当文件彻底读完后才执行 callback函数
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    const fileNameList = data.split(',');
+    console.log("fileNameList", fileNameList);
+    fileNameList.forEach(fileName => {
+      fs.readFile(`./txt/${fileName.trim()}.txt`, 'utf8', (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          fs.readFile(`./txt/${data.trim()}.txt`, 'utf8', (err, data) => {
+            if (err) return
+            console.log("data",data);
+          });
+        }
+      });
+    });
+  }
 });
 // 2.
 console.log("will read file!");
